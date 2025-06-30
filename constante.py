cf893951c72917f9157ea9491982a9c10436eb87
TomasOnesti <tomas.onesti.t1vl@gmail.com>
2025-06-30 15:10:40 -0300

    .

--- constante.py
+++ constante.py
@@ -26,37 +26,6 @@ def resource_path(relative_path):
 
     return os.path.join(base_path, relative_path)
 
-def pedir_codigo(pantalla, colores, fuente, fondo):
-        texto = ""
-        escribiendo = True
-        while escribiendo:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    exit()
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RETURN:
-                        escribiendo = False
-                    elif event.key == pygame.K_BACKSPACE:
-                        texto = texto[:-1]
-                    else:
-                        if len(texto) < 12:
-                            texto += event.unicode
-
-            pantalla.blit(fondo,(0,0))
-            mensaje = fuente.render("Ingresá el código secreto:", True, colores.WHITE)
-            input_texto = fuente.render(texto, True, colores.YELLOW)
-            rect_mensaje = mensaje.get_rect(center=(ANCHO/2, ALTO * 0.3))
-            rect_input = input_texto.get_rect(center=(ANCHO/2, ALTO * 0.5))
-
-            pantalla.blit(mensaje, rect_mensaje)
-            pantalla.blit(input_texto, rect_input)
-
-            pygame.display.flip()
-            pygame.time.Clock().tick(30)
-
-        return texto
-
 def mostrar_ranking(pantalla, colores, fuente, fondo, db):
     with open(db.archivo, 'r') as f:
         datos = json.load(f)
